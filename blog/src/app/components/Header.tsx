6bdfe66ded6d40ea0b8128a5f3c9b2862a22aca4
catmaker <rzt0317@nate.com>
2024-05-16 01:53:07 +0900

    feat : 헤더 로고 링크 추가

--- blog/src/app/components/Header.tsx
+++ blog/src/app/components/Header.tsx
@@ -5,7 +5,15 @@ const Header = () => {
   const menuItems = ["New", "Categories", "Reviews", "Contact"];
   return (
     <div className="flex items-center mt-6 mb-6">
-      <Image src={"img/catmaker.svg"} alt="logo" width={70} height={70}></Image>
+      <Link href={`/`}>
+        <Image
+          className="cursor-pointer hover:scale-90 duration-150"
+          src={"img/catmaker.svg"}
+          alt="logo"
+          width={70}
+          height={70}
+        ></Image>
+      </Link>
       <ul className="flex w-full gap-8 ml-8 ">
         {menuItems.map((item, index) => (
           <Link key={index} href={`/${item.toLowerCase()}`}>
@@ -16,7 +24,7 @@ const Header = () => {
         ))}
       </ul>
       <Image
-        className="cursor-pointer hover:scale-75 duration-150"
+        className="cursor-pointer hover:scale-90 duration-150"
         src={"img/search.svg"}
         alt="search_icon"
         width={27}
