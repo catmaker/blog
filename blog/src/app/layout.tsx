2cf398bf2ce8ec55b69d2bfdfbd81317ad2119e7
catmaker <rzt0317@nate.com>
2024-05-18 00:14:52 +0900

    style : 반응형 진행중

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import Nav from "@/app/components/Nav";
-import Footer from "@/app/components/Footer";
-import AuthContext from "./context/AuthContext";
+import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
+import AuthContext from "../context/AuthContext";
 export const metadata: Metadata = {
   title: "catmaker blog",
   description: "안녕하세요 catmaker 블로그입니다.",
@@ -16,13 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="font-bold">
-      <body className="w-4/5 mx-auto">
+      <body className="w-full ml-8 mr-8 lg:mx-auto">
         <AuthContext>
-          <header className="sticky top-0 z-100">
+          <header className="w-4/5 lg:mx-auto sticky top-0 z-100">
             <Nav></Nav>
           </header>
-          {children}
-          <Footer></Footer>
+          <article className="w-4/5 lg:mx-auto">{children}</article>
+          <footer className="w-4/5 lg:mx-auto">
+            <Footer></Footer>
+          </footer>
         </AuthContext>
       </body>
     </html>
