06094bcf8b54db2f9587c2e1bceca95455745245
catmaker <rzt0317@nate.com>
2024-05-17 20:58:35 +0900

    feat : NextAuth 추가 , AuthContext 생성

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 import Nav from "@/app/components/Nav";
 import Footer from "@/app/components/Footer";
-
+import AuthContext from "./context/AuthContext";
 export const metadata: Metadata = {
   title: "catmaker blog",
   description: "안녕하세요 catmaker 블로그입니다.",
@@ -17,11 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="font-bold">
       <body className="w-4/5 mx-auto">
-        <header className="sticky top-0 z-100">
-          <Nav></Nav>
-        </header>
-        {children}
-        <Footer></Footer>
+        <AuthContext>
+          <header className="sticky top-0 z-100">
+            <Nav></Nav>
+          </header>
+          {children}
+          <Footer></Footer>
+        </AuthContext>
       </body>
     </html>
   );
