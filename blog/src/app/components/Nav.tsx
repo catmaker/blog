9e7ed7f1e952fe254d750c6e3e23cee00c879768
catmaker <rzt0317@nate.com>
2024-05-17 20:59:07 +0900

    feat : sign in , out 로그인 현황따라 변경

--- blog/src/app/components/Nav.tsx
+++ blog/src/app/components/Nav.tsx
@@ -5,9 +5,11 @@ import LogoIcon from "./ui/icons/navIcons/LogoIcon";
 import SearchIcon from "./ui/icons/navIcons/SearchIcon";
 import BasicButton from "./ui/button/BasicButton";
 import { usePathname } from "next/navigation";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Nav = () => {
   const pathname = usePathname();
+  const { data: session } = useSession();
   console.log(pathname);
   const menuItems = ["New", "Categories", "Reviews", "Contact"];
   return (
@@ -29,11 +31,19 @@ const Nav = () => {
             </li>
           </Link>
         ))}
-        <BasicButton
-          className="px-4 py-2 rounded-lg hover:bg-pink-500 hover:text-white transition-colors duration-150"
-          text="Sign in"
-          onClick={() => {}}
-        />
+        {session ? (
+          <BasicButton
+            className="hover:bg-pink-500 hover:text-white px-4 py-2 rounded-lg transition-colors duration-150 text-base"
+            text="Sign Out"
+            onClick={() => signOut()}
+          />
+        ) : (
+          <BasicButton
+            className="hover:bg-pink-500 hover:text-white px-4 py-2 rounded-lg transition-colors duration-150 text-base"
+            text="Sign In"
+            onClick={() => signIn()}
+          />
+        )}
       </ul>
       <Link href={`/search`}>
         <SearchIcon className="cursor-pointer hover:scale-90 duration-150" />
