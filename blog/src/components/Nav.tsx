df6067989fcada0ef6f56e5a7cef60e620f390bf
catmaker <rzt0317@nate.com>
2024-05-18 00:15:20 +0900

    fix : components 폴더 위치 변경

--- blog/src/components/Nav.tsx
+++ blog/src/components/Nav.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import LogoIcon from "./ui/icons/navIcons/LogoIcon";
-import SearchIcon from "./ui/icons/navIcons/SearchIcon";
-import BasicButton from "./ui/button/BasicButton";
+import LogoIcon from "./ui/icons/LogoIcon";
+import SearchIcon from "./ui/icons/SearchIcon";
+import BasicButton from "@/components/ui/button/BasicButton";
 import { usePathname } from "next/navigation";
 import { useSession, signIn, signOut } from "next-auth/react";
 
@@ -15,13 +15,17 @@ const Nav = () => {
   return (
     <div className="flex items-center mt-6 mb-6">
       <Link href={`/`}>
-        <LogoIcon className="cursor-pointer hover:scale-90 duration-150" />
+        <LogoIcon
+          className="cursor-pointer hover:scale-90 duration-150 min-w-[50px]"
+          width={70}
+          height={70}
+        />
       </Link>
-      <ul className="flex w-full gap-8 ml-8 ">
+      <ul className="flex gap-8 ml-8 w-full">
         {menuItems.map((item, index) => (
           <Link key={index} href={`/${item.toLowerCase()}`}>
             <li
-              className={`hover:bg-pink-500 hover:text-white px-4 py-2 rounded-lg  transition-colors duration-150 text-base ${
+              className={`hover:bg-pink-500 hover:text-white  px-2 sm:px-4 py-1 sm:py-2  rounded-lg transition-colors duration-150 ${
                 pathname === `/${item.toLowerCase()}`
                   ? "bg-pink-500 text-white"
                   : ""
@@ -33,20 +37,24 @@ const Nav = () => {
         ))}
         {session ? (
           <BasicButton
-            className="hover:bg-pink-500 hover:text-white px-4 py-2 rounded-lg transition-colors duration-150 text-base"
+            className="hover:bg-pink-500 hover:text-white  px-2 sm:px-4 py-1 sm:py-2  rounded-lg transition-colors duration-150  whitespace-nowrap"
             text="Sign Out"
             onClick={() => signOut()}
           />
         ) : (
           <BasicButton
-            className="hover:bg-pink-500 hover:text-white px-4 py-2 rounded-lg transition-colors duration-150 text-base"
+            className="hover:bg-pink-500 hover:text-white  px-2 sm:px-4 py-1 sm:py-2  rounded-lg transition-colors duration-150  whitespace-nowrap"
             text="Sign In"
             onClick={() => signIn()}
           />
         )}
       </ul>
       <Link href={`/search`}>
-        <SearchIcon className="cursor-pointer hover:scale-90 duration-150" />
+        <SearchIcon
+          className="cursor-pointer hover:scale-90 duration-150 min-w-[20px]"
+          width={27}
+          height={27}
+        />
       </Link>
     </div>
   );
